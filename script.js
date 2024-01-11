3e8cc89f5294be57686e39d37ce783db9717576b
Adityanand Singh <adityanandsingh@outlook.com>
2024-01-11 15:29:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
+
+function cleanInputString(str) {
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');
+  }
+  
+  function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+  }
+  
